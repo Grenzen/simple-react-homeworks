af57722bc25203fcd08ed7dfe895ea9a583f46a4
Grenzen <grenzentur@rambler.ru>
2021-09-21 00:18:50 +0300

    feat: complete HW9

--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import Clock from './Clock'
+import d from '../../p1-main/m1-ui/u1-app/App.module.css'
 
 function HW9() {
     return (
         <div>
-            <hr/>
-            homeworks 9
-
-            {/*should work (должно работать)*/}
+            <h2 className={ d.hwTitle }>homework 9</h2>
             <Clock/>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeClock/>*/}
-            <hr/>
         </div>
     )
 }
